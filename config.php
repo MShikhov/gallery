9402f606652b5e06b03794abb497a03c35a9fd94
Misha Shikhov <Mishamoskovsky@gmail.com>
2021-01-06 12:56:37 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('URL', 'http://localhost/php_gallery'); // URL текущей страницы
+define('UPLOAD_MAX_SIZE', 1000000); // 1mb
+const ALLOWED_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
+define('UPLOAD_DIR', 'uploads');
+define('COMMENT_DIR', 'comments');
